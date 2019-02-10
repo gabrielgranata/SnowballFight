009a30e3760bca9aafffac9c4e4b817911e98503
Gabriel Granata <ggran067@uottawa.ca>
2019-02-10 11:46:58 -0500

    added score keeping ability

--- SnowballFight/Assets/Scripts/SnowBall.cs
+++ SnowballFight/Assets/Scripts/SnowBall.cs
@@ -23,6 +23,14 @@ public class SnowBall : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (collision.tag == "Player1")
+        {
+            FindObjectOfType<GameManager>().HurtP1();
+        } else if (collision.tag == "Player2")
+        {
+            FindObjectOfType<GameManager>().HurtP2();
+        }
         Instantiate(snowBallEffect, transform.position, transform.rotation);
         Destroy(gameObject);
     }
